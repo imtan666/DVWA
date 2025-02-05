178d515b59cbf704b43587dc409984e75fabf208
imtan666 <91042305+imtan666@users.noreply.github.com>
2025-02-05 16:56:22 +0700

    Update low.php

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -1,56 +1,58 @@
 <?php
 
-if( isset( $_REQUEST[ 'Submit' ] ) ) {
-	// Get input
-	$id = $_REQUEST[ 'id' ];
+if (isset($_REQUEST['Submit'])) {
+    // Get input
+    $id = $_REQUEST['id'];
 
-	switch ($_DVWA['SQLI_DB']) {
-		case MYSQL:
-			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+    switch ($_DVWA['SQLI_DB']) {
+        case MYSQL:
+            // Check database with Prepared Statement
+            $stmt = $GLOBALS["___mysqli_ston"]->prepare("SELECT first_name, last_name FROM users WHERE user_id = ?");
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+            $result = $stmt->get_result() or die('<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : ((($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false))) . '</pre>');
 
-			// Get results
-			while( $row = mysqli_fetch_assoc( $result ) ) {
-				// Get values
-				$first = $row["first_name"];
-				$last  = $row["last_name"];
+            // Get results
+            while ($row = $result->fetch_assoc()) {
+                // Get values
+                $first = $row["first_name"];
+                $last  = $row["last_name"];
 
-				// Feedback for end user
-				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-			}
+                // Feedback for end user
+                $html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+            }
 
-			mysqli_close($GLOBALS["___mysqli_ston"]);
-			break;
-		case SQLITE:
-			global $sqlite_db_connection;
+            $stmt->close();
+            mysqli_close($GLOBALS["___mysqli_ston"]);
+            break;
 
-			#$sqlite_db_connection = new SQLite3($_DVWA['SQLITE_DB']);
-			#$sqlite_db_connection->enableExceptions(true);
+        case SQLITE:
+            global $sqlite_db_connection;
 
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			#print $query;
-			try {
-				$results = $sqlite_db_connection->query($query);
-			} catch (Exception $e) {
-				echo 'Caught exception: ' . $e->getMessage();
-				exit();
-			}
+            try {
+                // Use Prepared Statement for SQLite
+                $stmt = $sqlite_db_connection->prepare("SELECT first_name, last_name FROM users WHERE user_id = :id");
+                $stmt->bindValue(":id", $id, SQLITE3_TEXT);
+                $results = $stmt->execute();
+            } catch (Exception $e) {
+                echo 'Caught exception: ' . $e->getMessage();
+                exit();
+            }
 
-			if ($results) {
-				while ($row = $results->fetchArray()) {
-					// Get values
-					$first = $row["first_name"];
-					$last  = $row["last_name"];
+            if ($results) {
+                while ($row = $results->fetchArray()) {
+                    // Get values
+                    $first = $row["first_name"];
+                    $last  = $row["last_name"];
 
-					// Feedback for end user
-					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-				}
-			} else {
-				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
-			}
-			break;
-	} 
+                    // Feedback for end user
+                    $html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+                }
+            } else {
+                echo "Error in fetch " . $sqlite_db_connection->lastErrorMsg();
+            }
+            break;
+    }
 }
 
 ?>
